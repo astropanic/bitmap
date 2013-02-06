a1edfc5f3e93c8093bdcb284016fb9bfb5076603
Wojciech Pietrzak <act.as.coder@gmail.com>
2013-02-06 20:15:24 +0100

    neighbours function

--- spec/BitmapSpec.js
+++ spec/BitmapSpec.js
@@ -1,11 +1,38 @@
 describe("Bitmap", function() {
   var grid;
 
-  beforeEach(function() {
+  it("should find closed path", function() {
+    var map = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0],
+    [0,0,1,1,1,1,1,0],
+    [0,0,1,0,0,0,1,0],
+    [0,0,1,1,0,0,1,0],
+    [0,0,1,0,0,0,1,0],
+    [0,0,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0]
+    ];
+  bitmap = new Bitmap(map);
+  expect(bitmap.closedPath()).toEqual(true);
+  });
 
+  it("should find closed path", function() {
+    var map = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0],
+    [0,0,1,1,1,1,1,0],
+    [0,0,1,0,0,0,1,0],
+    [0,0,1,1,0,0,0,0],
+    [0,0,1,0,0,0,1,0],
+    [0,0,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0]
+    ];
+  bitmap = new Bitmap(map);
+  expect(bitmap.closedPath()).toEqual(true);
+  });
 
-
-  var map3 = [
+  it("should not find any closed path", function() {
+    var map = [
     [0,1,1,0,0,0,0,0],
     [0,1,1,0,0,0,0,0],
     [0,1,1,0,0,0,0,0],
@@ -14,9 +41,13 @@ describe("Bitmap", function() {
     [0,1,1,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0]
-  ];
+    ];
+  bitmap = new Bitmap(map);
+  expect(bitmap.closedPath()).toEqual(false);
+  });
 
-  var map4 = [
+  it("should not find any closed path", function() {
+    var map = [
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,1,0,0,0],
     [0,0,1,1,1,1,1,0],
@@ -25,67 +56,20 @@ describe("Bitmap", function() {
     [0,0,1,0,0,0,1,0],
     [0,0,1,1,1,1,1,0],
     [0,0,0,0,0,0,0,0]
-  ];
-
+    ];
+  bitmap = new Bitmap(map);
+  expect(bitmap.closedPath()).toEqual(false);
   });
 
-  it("should find closed path", function() {
+  it("should return 3 neighbours from a corner", function() {
     var map = [
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,0,0,0],
-        [0,0,1,1,1,1,1,0],
-        [0,0,1,0,0,0,1,0],
-        [0,0,1,1,0,0,1,0],
-        [0,0,1,0,0,0,1,0],
-        [0,0,1,1,1,1,1,0],
-        [0,0,0,0,0,0,0,0]
+    [0,1,2,3],
+    [4,5,6,7],
+    [8,9,0,1],
+    [2,3,4,5]
     ];
-    bitmap = new Bitmap(map);
-    expect(bitmap.closedPath()).toEqual(true);
-  });
 
-  it("should find closed path", function() {
-    var map = [
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,0,0,0],
-        [0,0,1,1,1,1,1,0],
-        [0,0,1,0,0,0,1,0],
-        [0,0,1,1,0,0,0,0],
-        [0,0,1,0,0,0,1,0],
-        [0,0,1,1,1,1,1,0],
-        [0,0,0,0,0,0,0,0]
-    ];
-    bitmap = new Bitmap(map);
-    expect(bitmap.closedPath()).toEqual(true);
-  });
-
-  it("should not find any closed path", function() {
-    var map = [
-        [0,1,1,0,0,0,0,0],
-        [0,1,1,0,0,0,0,0],
-        [0,1,1,0,0,0,0,0],
-        [0,1,1,0,0,0,0,0],
-        [0,1,1,0,0,0,0,0],
-        [0,1,1,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0]
-    ];
-    bitmap = new Bitmap(map);
-    expect(bitmap.closedPath()).toEqual(false);
-  });
-
-  it("should not find any closed path", function() {
-    var map = [
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,1,0,0,0],
-        [0,0,1,1,1,1,1,0],
-        [0,0,1,0,0,0,1,0],
-        [0,0,1,0,0,1,0,0],
-        [0,0,1,0,0,0,1,0],
-        [0,0,1,1,1,1,1,0],
-        [0,0,0,0,0,0,0,0]
-    ];
-    bitmap = new Bitmap(map);
-    expect(bitmap.closedPath()).toEqual(false);
+  bitmap = new Bitmap(map);
+  expect(bitmap.neighbours(0,0)).toEqual([4,1,5]);
   });
 });
--- src/bitmap.js
+++ src/bitmap.js
@@ -1,5 +1,18 @@
 var Bitmap = function (map) {
+  this.map  = map;
+  this.size = map[0].length,
   this.closedPath = function(){
     return true;
+  },
+  this.neighbours = function(x, y) {
+    cells = new Array();
+    for(yy = y-1; yy <= y+1; yy ++){
+      for(xx = x-1; xx <= x+1; xx ++){
+        if(xx>=0 && xx <= this.size && yy>=0 && yy <= this.size && (xx != x || yy != y)){
+          cells.push(this.map[xx][yy]);
+        }
+      }
+    }
+    return cells;
   }
 };
