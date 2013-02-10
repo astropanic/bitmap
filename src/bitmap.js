1d5a75eb4d535f5edeca9139cfc5d65f7d5f6f39
Wojciech Pietrzak <act.as.coder@gmail.com>
2013-02-11 00:09:04 +0100

    finding connections is working

--- src/bitmap.js
+++ src/bitmap.js
@@ -8,14 +8,11 @@ var Bitmap = function (map) {
     return true;
   },
   this.neighbours = function(x, y) {
-    tmp = x;
-    x = y;
-    y = tmp;
     cells = new Array();
     for(yy = y-1; yy <= y+1; yy ++){
       for(xx = x-1; xx <= x+1; xx ++){
         if(xx>=0 && xx <= this.size && yy>=0 && yy <= this.size && (xx != x || yy != y)){
-          cells.push( {x: xx, y: yy, value: this.map(xx, yy) });
+          cells.push( {x: xx, y: yy, value: this.map(xx,yy) });
         }
       }
     }
